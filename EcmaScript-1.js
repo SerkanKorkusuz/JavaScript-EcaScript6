dbc87c505bcefd8a09dc58237d564cbf6575edcb
Serkan KORKUSUZ <42834460+SerkanKorkusuz@users.noreply.github.com>
2018-12-08 23:25:55 +0300

    Create EcmaScript-1.js

---
+++ EcmaScript-1.js
@@ -0,0 +1,18 @@
+arr = [1, 2, 3, 4, 5, 6];
+
+arr2 = arr.map((num) => {return num * 2;});
+arr2;
+
+arr3 = arr.map(num => {return num *3});
+arr3;
+
+arr4 = arr.map(num => num*4);
+arr4;
+
+arr_class = arr.map(num => (
+    { num : num*2, 
+      }));
+arr_class;
+
+arr_of_twos = arr.map(() => 2);
+arr_of_twos;
