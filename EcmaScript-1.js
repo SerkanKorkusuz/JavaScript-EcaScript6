783c1121e6701a4efc89339cb9badc03979f722d
Serkan KORKUSUZ <42834460+SerkanKorkusuz@users.noreply.github.com>
2018-12-08 23:27:38 +0300

    Update EcmaScript-1.js

--- EcmaScript-1.js
+++ EcmaScript-1.js
@@ -1,3 +1,5 @@
+const javascript_bender = "serkan korkusuz";
+
 arr = [1, 2, 3, 4, 5, 6];
 
 arr2 = arr.map((num) => {return num * 2;});
@@ -16,3 +18,15 @@ arr_class;
 
 arr_of_twos = arr.map(() => 2);
 arr_of_twos;
+
+var obj = {
+    value : 0,
+    increment : function(){
+        setTimeout(() =>  {
+            this.value++;
+            console.log(this.value);
+        }, 1000)
+    }
+}
+
+obj.increment()
