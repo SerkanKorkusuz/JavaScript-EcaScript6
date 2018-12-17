9dc77a358c637976a5e72a259407aa521f85b817
Serkan KORKUSUZ <42834460+SerkanKorkusuz@users.noreply.github.com>
2018-12-17 22:34:11 +0300

    Update EcmaScript-2.js

--- EcmaScript-2.js
+++ EcmaScript-2.js
@@ -5,3 +5,11 @@ var age = 22;
 
 var message = `Hello. My name is ${name}. I am ${age} years old.`
 message;
+
+var text = "Hello! My name is \n Bob. What is \n your name?";
+console.log(text);
+
+var text2 = `Hello! My name is 
+    Bob.        What is
+yours?`;
+console.log(text2);
