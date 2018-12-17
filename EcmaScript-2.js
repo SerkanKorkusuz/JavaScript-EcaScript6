c9067711476639bab56a239eaafdd3eec8fe9921
Serkan KORKUSUZ <42834460+SerkanKorkusuz@users.noreply.github.com>
2018-12-17 22:34:49 +0300

    Update EcmaScript-2.js

--- EcmaScript-2.js
+++ EcmaScript-2.js
@@ -13,3 +13,14 @@ var text2 = `Hello! My name is
     Bob.        What is
 yours?`;
 console.log(text2);
+
+
+const activities = [
+    "running",
+    "swimming",
+    "crossing",
+    "hiking"
+];
+
+const myMess = `I like many activities and the ones I mostly spend time for doing are ${activities.join(", ")}`;
+console.log(myMess);
