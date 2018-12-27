b56bf77662e3d44a57544d6539df7c1358c0273f
Serkan KORKUSUZ <42834460+SerkanKorkusuz@users.noreply.github.com>
2018-12-27 23:57:53 +0300

    Update EcmaScript-3.js

--- EcmaScript-3.js
+++ EcmaScript-3.js
@@ -1,3 +1,12 @@
 const last_javascript_bender = "serkan korkusuz";
 
+class Person{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    jump(){
+        console.log("jump");
+    }
+
 #to be continued...
